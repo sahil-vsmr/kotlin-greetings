9ae98da5b1ac414d3d49bd3d1a99437bec622eca
Sahil Agarwal <80445777+Sahil-Agarwal-DD@users.noreply.github.com>
2022-08-03 16:57:51 +0530

    Hello World.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun printMessage(message: String) {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x+y
+}
+
+fun multiply(x: Int, y: Int) = x*y
+
+fun main() {
+    println("What is your name?")
+    val name = readln()
+    println("Hello $name")
+
+    printMessage("Hello")
+    printMessageWithPrefix("Hello", "Log")
+    printMessageWithPrefix("Hello")
+    printMessageWithPrefix(prefix = "Log", message = "Hello")
+
+    println(sum(1,2))
+    println(multiply(2,4))
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //println("Program arguments: ${args.joinToString()}")
 }
